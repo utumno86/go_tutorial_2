cc56b7cc07761e6b6c7b45466b716547acf2bc12
mnash <utumno86@gmail.com>
2018-02-19 11:37:10 -0500

    added a sub-function

--- Cards/main.go
+++ Cards/main.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	card := "Ace of Spades"
-	card = "Five of Diamonds"
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
