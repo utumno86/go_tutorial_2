e81c617ee9bcab7d44b8f6f32e20722c5c5aa2b7
mnash <utumno86@gmail.com>
2018-03-19 00:00:00 -0400

    cleanup

--- Cards/deck.go
+++ Cards/deck.go
@@ -12,8 +12,8 @@ func newDeck() deck {
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
-	for i, suit := range cardSuits {
-		for j, value := range cardValues {
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
 			cards = append(cards, value+" of "+suit)
 		}
 	}
@@ -22,7 +22,7 @@ func newDeck() deck {
 }
 
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
 }
