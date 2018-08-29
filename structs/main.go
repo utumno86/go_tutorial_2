ccd9ad40ad80677a593ff623385643f544a23a23
Michael Nash <michael@spatialnetworks.com>
2018-08-29 12:26:55 -0400

    fun with structs

---
+++ structs/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@party.com",
+			zipCode: 90210,
+		},
+	}
+	fmt.Printf("%+v", jim)
+}
